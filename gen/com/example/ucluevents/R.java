84c4b5cca975a001fc383314170d8d8ae71075bf
lauragagliardi <laura93.cobianchi@libero.it>
2013-10-26 18:38:27 +0100

    kkkk

--- gen/com/example/ucluevents/R.java
+++ gen/com/example/ucluevents/R.java
@@ -22,14 +22,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
+        public static final int repeat_bg=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int btnLogin=0x7f080002;
+        public static final int btnRegister=0x7f080007;
+        public static final int footer=0x7f080001;
+        public static final int header=0x7f080000;
+        public static final int link_to_login=0x7f080008;
+        public static final int link_to_register=0x7f080003;
+        public static final int reg_email=0x7f080005;
+        public static final int reg_fullname=0x7f080004;
+        public static final int reg_password=0x7f080006;
     }
     public static final class layout {
         public static final int activity_sign_in=0x7f030000;
-        public static final int header_gradient=0x7f030001;
-        public static final int login=0x7f030002;
+        public static final int footer_repeat=0x7f030001;
+        public static final int header_gradient=0x7f030002;
+        public static final int login=0x7f030003;
+        public static final int register=0x7f030004;
     }
     public static final class menu {
         public static final int sign_in=0x7f070000;
