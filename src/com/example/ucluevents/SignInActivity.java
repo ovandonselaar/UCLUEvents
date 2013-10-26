19ec0571cc91a13b2975b7901cd1c19e3e259bd2
lauragagliardi <laura93.cobianchi@libero.it>
2013-10-26 18:04:40 +0100

    uclu

--- src/com/example/ucluevents/SignInActivity.java
+++ src/com/example/ucluevents/SignInActivity.java
@@ -1,22 +1,28 @@
 package com.example.ucluevents;
-
-import android.os.Bundle;
+ 
 import android.app.Activity;
-import android.view.Menu;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+ 
 public class SignInActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sign_in);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.sign_in, menu);
-		return true;
-	}
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setting default screen to login.xml
+        setContentView(R.layout.login);
+ 
+        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+ 
+        // Listening to register new account link
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View v) {
+                // Switching to Register screen
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
